77159f0848d7e3b104425f9f3a93e429c4773115
Matthies Abera <s3779748@student.rmit.edu.au>
2023-10-21 14:38:57 +1100

    added testing libraries.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import styles from './page.module.css'
 import AboutMe from '@/components/AboutMe'
 import Contact from '@/components/Contact'
 import Hero from '@/components/Hero'
@@ -7,7 +6,7 @@ import Projects from '@/components/Projects'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
       <Navbar />
       <Hero />
       <AboutMe />
